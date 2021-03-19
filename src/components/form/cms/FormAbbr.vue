49c38ef98cd4f42876f90dc2af38b23ce79666db
michaelwang <wangqiangshen@gmail.com>
2021-03-19 17:35:58 +0800

    update abbr

--- src/components/form/cms/FormAbbr.vue
+++ src/components/form/cms/FormAbbr.vue
@@ -1,217 +1,127 @@
 <template>
-  <v-card>
-    <v-card-text>
-      <v-form>
-        <v-container fluid>
-          <v-row>
-            <v-col :cols="6">
-              <v-text-field
-                v-model="formModel.name"
-                outlined
-                name="name"
-                placeholder="Name"
-                label="Name"
-                @input="handleInputName"
-                append-icon="mdi-translate"
-                @click:append="handleShowTranslation('name')"
-              />
-            </v-col>
-            <v-col :cols="6">
-              <v-text-field
-                v-model="formModel.slug"
-                outlined
-                placeholder="Slug"
-                label="Slug"
-                append-icon="mdi-eye"
-                @click:append="handleViewItem"
-              />
-            </v-col>
-            <v-col :cols="12">
-              <v-text-field
-                v-model="formModel.short_for"
-                outlined
-                placeholder="Short For"
-                label="Short For"
-              />
-            </v-col>
-            <v-col :cols="12">
-              <v-text-field
-                v-model="formModel.reference_url"
-                outlined
-                placeholder="Reference"
-                label="Reference"
-                append-icon="mdi-eye"
-                @click:append="handleViewReference(formModel.reference_url)"
-              />
-            </v-col>
-            <v-col :cols="6" v-if="item">
-              <label>Featured Image</label>
-              <image-holder
-                v-model="selectedMedia"
-                :entity="entity.model"
-                :entity-id="item.id"
-                tag="abbr"
-                @detached="selectedMedia = null"
-                @attached="handleAttached"
-              />
-            </v-col>
-            <v-col :cols="12">
-              <v-textarea
-                v-model="formModel.description"
-                outlined
-                placeholder="Description"
-                label="Description"
-                append-icon="mdi-translate"
-                @click:append="handleShowTranslation('description')"
-              />
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-    </v-card-text>
-    <v-card-actions class="py-3">
-      <v-spacer></v-spacer>
-      <v-btn :loading="loading" tile color="primary" @click="handleSubmit"
-        >save</v-btn
-      >
-    </v-card-actions>
-    <v-dialog v-model="showTranslation">
-      <form-translation
-        :field="translationField"
-        :text="translationText"
-        :entity="entity"
-        @form:cancel="showTranslation = false"
-        @form:success="showTranslation = false"
-      />
-    </v-dialog>
-  </v-card>
+  <v-form-builder
+    ref="builder"
+    v-model="formModel"
+    :title="formTitle"
+    :items="formItems"
+    :loading="loading"
+    color="primary"
+    show-header
+    @form:submit="handleSubmit"
+    @form:cancel="$emit('form:cancel')"
+  />
 </template>
 
 <script>
-import SlugifyMixin from '@/mixins/Slugify'
-import FormTranslation from '@/components/form/FormTranslation'
-import ImageHolder from '@/components/image/ImageHolder'
+import VFormBuilder from '@/components/builder/VFormBuilder'
+import { VTextField, VTextarea } from 'vuetify/lib'
 export default {
   name: 'FormAbbr',
-  mixins: [SlugifyMixin],
   components: {
-    FormTranslation,
-    ImageHolder
+    VFormBuilder,
   },
   props: {
-    item: Object
+    item: Object,
   },
   data() {
     return {
-      // image picker
-      selectedMedia: null,
-      //
-      showTranslation: false,
-      translationField: 'short_for',
-      translationText: null,
-      isLoading: false,
       loading: false,
-      formModel: {
-        name: null,
-        slug: null,
-        description: null,
-        short_for: null,
-        reference_url: null,
-        image: null
-      }
+      formModel: {},
     }
   },
   computed: {
-    entity() {
-      return {
-        model: 'App\\Models\\Abbreviationable\\Abbreviation',
-        id: this.item ? this.item.id : 0
-      }
+    formTitle() {
+      return this.item ? 'Edit Abbr - ' + this.item.name : 'Create Abbr'
     },
-    directory() {
-      return this.item ? `abbr/${this.item.id}` : `abbr`
-    }
+    formItems() {
+      return [
+        {
+          cols: 6,
+          element: VTextField,
+          props: {
+            name: 'name',
+            required: true,
+            outlined: true,
+            rules: [(v) => !!v || 'Name is required'],
+          },
+        },
+        {
+          cols: 6,
+          element: VTextField,
+          props: {
+            name: 'short_for',
+            required: true,
+            outlined: true,
+          },
+        },
+        {
+          cols: 6,
+          element: VTextField,
+          props: {
+            name: 'slug',
+            outlined: true,
+          },
+        },
+        {
+          cols: 6,
+          element: VTextField,
+          props: {
+            name: 'reference_url',
+            outlined: true,
+          },
+        },
+        {
+          cols: 12,
+          element: VTextarea,
+          props: {
+            name: 'description',
+            outlined: true,
+          },
+        },
+      ]
+    },
   },
   watch: {
     item: {
       handler(item) {
-        if (item) {
-          this.assignModel(item)
-        } else {
-          this.initForm()
-        }
+        this.formModel = item || {}
       },
-      immediate: true
-    }
+      immediate: true,
+    },
   },
   methods: {
-    initForm() {
-      this.formModel = {
-        name: null,
-        slug: null,
-        description: null,
-        short_for: null,
-        reference_url: null,
-        image: null
-      }
-    },
-    assignModel(data) {
-      for (let key in this.formModel) {
-        this.formModel[key] = data[key]
-      }
-    },
     handleSubmit() {
-      this.loading = true
-      if (this.item) {
-        this.$store
-          .dispatch('updateAbbr', {
-            id: this.item.id,
-            data: this.formModel
-          })
-          .then(() => {
-            this.loading = false
-          })
-          .catch(() => {
-            this.loading = false
-          })
-      } else {
-        this.$store
-          .dispatch('createAbbr', this.formModel)
-          .then(({ data }) => {
-            this.loading = false
-            this.$router.push({
-              path: `/cms/abbrs/item/${data.id}`
+      const form = this.$refs.builder.$refs.form
+      if (form.validate()) {
+        this.loading = true
+        const data = this.transformData(this.formModel)
+        if (this.item && this.item.id) {
+          return this.$store
+            .dispatch('updateAbbr', {
+              id: this.item.id,
+              data: data,
             })
-          })
-          .catch(() => {
-            this.loading = false
-          })
+            .then(() => {
+              this.loading = false
+            })
+            .catch(() => {
+              this.loading = false
+            })
+        } else {
+          return this.$store
+            .dispatch('createAbbr', data)
+            .then(() => {
+              this.loading = false
+            })
+            .catch(() => {
+              this.loading = false
+            })
+        }
       }
     },
-    handleViewItem() {
-      if (this.item) {
-        window.open(this.item.href, '_blank')
-      }
+    transformData(data) {
+      return data
     },
-    handleViewReference(url) {
-      if (url) {
-        window.open(url, '_blank')
-      }
-    },
-    handleInputName(name) {
-      return (this.formModel.slug = this.slugify(name))
-    },
-    handleShowTranslation(field) {
-      this.translationField = field
-      this.translationText = this.formModel[field]
-      this.showTranslation = true
-    },
-    //
-    handleAttached(media) {
-      this.selectedMedia = media
-    }
-  }
+  },
 }
 </script>
-
-<style></style>
--- src/views/cms/AbbrList.vue
+++ src/views/cms/AbbrList.vue
@@ -1,380 +1,130 @@
 <template>
-  <div class="cms-abbr-list">
+  <div class="cms-abbr__list">
     <v-container>
       <v-row>
         <v-col cols="12">
-          <v-card>
-            <v-toolbar flat>
-              <v-text-field
-                text
-                solo
-                flat
-                :prepend-icon="
-                  showFilter ? 'mdi-filter-variant-plus' : 'mdi-filter-variant'
-                "
-                append-icon="mdi-magnify"
-                :placeholder="__('search')"
-                v-model="filter['filter[name]']"
-                hide-details
-                clearable
-                @keyup.enter="handleApplyFilter"
-                @click:append="handleApplyFilter"
-                @click:prepend="showFilter = !showFilter"
-                @click:clear="handleClear"
-              />
-              <v-btn @click="handleRefreshItem" icon>
-                <v-icon>mdi-refresh</v-icon>
-              </v-btn>
-              <v-btn @click="handleCreateItem" icon>
-                <v-icon>mdi-plus</v-icon>
-              </v-btn>
-            </v-toolbar>
-            <v-divider />
-            <v-card v-show="showFilter" flat class="grey lighten-4">
-              <v-card-text>
-                <v-row>
-                  <v-col cols="6"> </v-col>
-                </v-row>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn @click="handleResetFilter" text>{{ __('reset') }}</v-btn>
-                <v-btn tile @click="handleApplyFilter" color="primary">{{
-                  __('apply')
-                }}</v-btn>
-              </v-card-actions>
-            </v-card>
-            <v-card-text class="pa-0">
-              <v-grid
-                :loading="loadingItems"
-                :headers="headers"
-                :items="items"
-                :footer-props="{ itemsPerPageOptions: [15, 30, 50] }"
-                :server-items-length="serverItemsLength"
-                :items-per-page.sync="filter['pageSize']"
-                :page.sync="filter['page']"
-                @update:page="handlePageChanged"
-                @update:items-per-page="handlePageSizeChanged"
-                item-key="id"
-                show-select
-              >
-                <template v-slot:[`item.media`]="{ item }">
-                  <v-img
-                    v-if="item.media && item.media.length > 0"
-                    class="ma-3"
-                    :src="item.media[0].tiny_image"
-                    width="50"
-                    height="50"
-                    @click="handleShowLightBox(item)"
-                  />
-                </template>
-                <template v-slot:[`item.short_for`]="{ item }">
-                  <div>
-                    <span>{{ item.short_for }}</span>
-                    <v-icon
-                      class="mx-2"
-                      @click="handleShowTranslation('short_for', item)"
-                      size="18"
-                      >mdi-translate</v-icon
-                    >
-                  </div>
-                  <div>
-                    <v-avatar
-                      class="mr-1"
-                      tile
-                      size="16"
-                      v-for="locale in getLocales"
-                      :key="locale.value"
-                    >
-                      <img
-                        :src="locale.svg"
-                        :class="
-                          hasTranslation(
-                            locale.value,
-                            item.translations.short_for
-                          )
-                        "
-                      />
-                    </v-avatar>
-                  </div>
-                </template>
-                <template v-slot:[`item.action`]="{ item }">
-                  <v-menu>
-                    <template v-slot:activator="{ on: menu }">
-                      <v-tooltip bottom>
-                        <template v-slot:activator="{ on: tooltip }">
-                          <v-btn icon v-on="onTooltip({ ...tooltip, ...menu })">
-                            <v-icon>mdi-dots-vertical</v-icon>
-                          </v-btn>
-                        </template>
-                        <span>{{ __('action') }}</span>
-                      </v-tooltip>
-                    </template>
-                    <v-list class="pa-0" dense>
-                      <v-list-item
-                        v-for="action in actions"
-                        :key="action.text"
-                        @click="action.click(item)"
-                      >
-                        <v-list-item-icon class="mr-2">
-                          <v-icon small>{{ action.icon }}</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-title>{{ action.text }}</v-list-item-title>
-                      </v-list-item>
-                    </v-list>
-                  </v-menu>
-                </template>
-              </v-grid>
-            </v-card-text>
-          </v-card>
+          <list-grid
+            ref="grid"
+            :headers="headers"
+            :filter-items="filterItems"
+            :actions="actions"
+            action="fetchAbbr"
+            search-field="name"
+            @create="handleCreateItem"
+          />
         </v-col>
       </v-row>
     </v-container>
-    <v-dialog v-model="showTranslationDialog">
-      <form-translation
-        :field="translationField"
-        :text="translationText"
-        :entity="entity"
-        @form:cancel="showTranslationDialog = false"
-        @form:success="handleTranslated"
-      />
-    </v-dialog>
-    <vue-easy-lightbox
-      :visible="showLightbox"
-      :imgs="images"
-      @hide="showLightbox = false"
-    />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import ResizeMixin from '@/mixins/Resize'
-import TooltipMixin from '@/mixins/Tooltip'
-import VGrid from '@/components/grid'
-import FormTranslation from '@/components/form/FormTranslation'
+import FormAbbr from '@/components/form/cms/FormAbbr'
+import ListGrid from '@/components/list/ListGrid'
+import { VSwitch } from 'vuetify/lib'
 export default {
+  name: 'PagePost',
   components: {
-    VGrid,
-    FormTranslation
+    ListGrid,
   },
-  mixins: [ResizeMixin, TooltipMixin],
   data() {
     return {
-      showLightbox: false,
-      images: [],
-      showTranslationDialog: false,
-      translationText: '',
-      translationField: 'name',
-      //filter
-      search: '',
-      showFilter: true,
-      filter: {
-        page: 1,
-        pageSize: 30,
-        'filter[is_active]': null,
-        'filter[name]': null
-      },
-      // table
-      selectedItem: null,
-      loadingItems: false,
-      serverItemsLength: 0,
       headers: [
         {
           text: this.__('id'),
-          value: 'id'
-        },
-        {
-          text: 'Media',
-          value: 'media'
+          value: 'id',
         },
         {
           text: 'Name',
-          value: 'name'
+          value: 'name',
+          sortable: true,
         },
         {
-          text: 'Short For',
-          value: 'short_for'
+          text: 'Media',
+          value: 'media',
+          sortable: false,
+        },
+        {
+          text: 'Short for',
+          value: 'short_for',
+          sortable: false,
         },
         {
           text: 'Action',
-          value: 'action'
-        }
+          value: 'action',
+          sortable: false,
+        },
       ],
       items: [],
       actions: [
         {
           text: 'View Item',
           icon: 'mdi-eye',
-          click: this.handleViewItem
+          click: this.handleViewItem,
         },
         {
           text: 'Edit Item',
           icon: 'mdi-pencil',
-          click: this.handleEditItem
+          click: this.handleEditItem,
+        },
+        {
+          text: 'Edit Value',
+          icon: 'mdi-pencil',
+          click: this.handleEditValue,
         },
         {
           text: 'Delete Item',
           icon: 'mdi-close',
-          click: this.handleDeleteItem
-        }
-      ]
+          click: this.handleDeleteItem,
+        },
+      ],
     }
   },
   computed: {
-    ...mapGetters(['getLocales', 'getTagTypes']),
-    entity() {
-      return {
-        model: 'App\\Models\\Abbreviationable\\Abbreviation',
-        id: this.selectedItem ? this.selectedItem.id : 0
-      }
-    }
+    filterItems() {
+      return []
+    },
   },
-  watch: {
-    '$route.query': {
-      handler(data) {
-        if (Object.keys(data).length === 0 && data.constructor === Object) {
-          this.resetFilter()
-          this.fetchRecords()
-        } else {
-          const query = this.updateFilterQuery(data)
-          this.fetchRecords(query)
-        }
-      },
-      immediate: true
-    }
-  },
-  created() {},
+  watch: {},
   methods: {
-    //
-    updateFilterQuery(query) {
-      const filter = Object.assign(this.filter, query)
-      filter.page = parseInt(filter.page)
-      filter.pageSize = parseInt(filter.pageSize)
-      return filter
-    },
-    resetFilter() {
-      this.filter = {
-        page: 1,
-        pageSize: 30,
-        'fitler[is_active]': null,
-        'filter[name]': null
-      }
-    },
-    fetchRecords(query) {
-      this.loadingItems = true
-      this.items = []
-      return this.$store
-        .dispatch('fetchAbbr', query)
-        .then(({ data, meta }) => {
-          this.items = data
-          this.serverItemsLength = meta.total
-          this.loadingItems = false
-        })
-        .catch(() => {
-          this.loadingItems = false
-        })
-    },
-    handleItemStatus(key, val, id) {
-      let data = {}
-      data[key] = val
-      this.$store
-        .dispatch('updateAbbr', {
-          id: id,
-          data: data
-        })
-        .then(() => {})
-    },
     //action
     handleCreateItem() {
-      this.$router.push({
-        path: `/cms/abbrs/create`
+      const dialog = this.$root.$dialog
+      dialog.loadComponent({
+        component: FormAbbr,
+        data: {
+          item: null,
+        },
+        on: {
+          'form:cancel': () => {
+            dialog.hide()
+          },
+        },
       })
-    },
-    handleViewItem(item) {
-      window.open(item.href, '_blank')
+      dialog.show()
     },
     handleEditItem(item) {
-      this.$router.push({
-        path: `/cms/abbrs/item/${item.id}`
+      const dialog = this.$root.$dialog
+      dialog.loadComponent({
+        component: FormAbbr,
+        data: {
+          item: item,
+        },
+        on: {
+          'form:cancel': () => {
+            dialog.hide()
+          },
+        },
       })
+      dialog.show()
     },
     handleDeleteItem({ id }) {
       if (window.confirm('Are you sure to delete this item ?')) {
-        this.$store.dispatch('deleteAbbr', id).then(() => {
-          this.items = this.items.filter((item) => item.id !== id)
+        this.$store.dispatch('deleteUser', id).then(() => {
+          this.$refs.grid.fetchRecords()
         })
       }
     },
-    handleRefreshItem() {
-      this.fetchRecords(this.filter)
-    },
-    // filter
-    handlePageChanged(page) {
-      this.filter.page = page
-      this.filter.t = Date.now()
-      this.$router.replace({
-        path: this.$route.path,
-        query: this.filter
-      })
-    },
-    handlePageSizeChanged(size) {
-      this.filter.pageSize = size
-      this.filter.t = Date.now()
-      this.$router.replace({
-        path: this.$route.path,
-        query: this.filter
-      })
-    },
-    handleResetFilter() {
-      this.resetFilter()
-      this.$router.replace({
-        path: this.$route.path
-      })
-    },
-    handleApplyFilter() {
-      this.filter.t = Date.now()
-      this.$router.replace({
-        path: this.$route.path,
-        query: this.filter
-      })
-    },
-    handleClear() {
-      this.resetFilter()
-      this.filter.t = Date.now()
-      this.$router.replace({
-        path: this.$route.path,
-        query: this.filter
-      })
-    },
-    //
-    hasTranslation(locale, translations) {
-      const grey = translations[locale] ? false : true
-      return {
-        'grey-bg': grey
-      }
-    },
-    handleShowTranslation(field, item) {
-      this.translationField = field
-      this.translationText = item[field]
-      this.selectedItem = item
-      this.showTranslationDialog = true
-    },
-    handleTranslated() {
-      this.showTranslationDialog = false
-      this.fetchRecord(this.filter)
-    },
-    //lightbox
-    handleShowLightBox(item) {
-      console.log(item)
-      this.images = [
-        {
-          src: item.image,
-          title: item.name
-        }
-      ]
-      this.showLightbox = true
-    }
-  }
+  },
 }
 </script>
