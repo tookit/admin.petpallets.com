78b5cd3b6f7d6e0c34761a77b9598ea5f516e7a4
michaelwang <wangqiangshen@gmail.com>
2021-03-19 13:16:32 +0800

    refactor user list

---
+++ src/components/list/ListGrid.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="list-grid">
+    <v-sheet class="list-grid__filter">
+      <v-toolbar flat>
+        <v-text-field
+          v-model="search"
+          text
+          solo
+          flat
+          :prepend-icon="
+            showFilter ? 'mdi-filter-variant-plus' : 'mdi-filter-variant'
+          "
+          append-icon="mdi-magnify"
+          :placeholder="__('search')"
+          hide-details
+          clearable
+          @keyup.enter="handleApplyFilter"
+          @click:append="handleApplyFilter"
+          @click:prepend="showFilter = !showFilter"
+          @click:clear="handleClear"
+        />
+        <v-btn icon @click="handleRefreshItem">
+          <v-icon>mdi-refresh</v-icon>
+        </v-btn>
+        <v-btn icon @click="$emit('create')">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-divider />
+      <v-sheet v-show="showFilter">
+        <v-form-builder
+          class="grey lighten-4"
+          :items="filterItems"
+          color="primary"
+          @form:cancel="handleResetFilter"
+          @form:submit="handleApplyFilter"
+        />
+      </v-sheet>
+    </v-sheet>
+    <v-grid
+      :loading="loadingItems"
+      :headers="headers"
+      :items="items"
+      :footer-props="{ itemsPerPageOptions: [15, 30, 50] }"
+      :server-items-length="serverItemsLength"
+      :options.sync="gridOptions"
+      item-key="id"
+      show-select
+      @update:items-per-page="handleApplyFilter"
+      @update:page="handleApplyFilter"
+      @update:sort-by="handleApplyFilter"
+    >
+      <template #[`item.action`]="{ item }">
+        <v-menu>
+          <template #activator="{ on: menu }">
+            <v-tooltip bottom>
+              <template #activator="{ on: tooltip }">
+                <v-btn icon v-on="onTooltip({ ...tooltip, ...menu })">
+                  <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </template>
+              <span>{{ __('action') }}</span>
+            </v-tooltip>
+          </template>
+          <v-list class="pa-0" dense>
+            <v-list-item
+              v-for="act in actions"
+              :key="act.text"
+              @click="act.click(item)"
+            >
+              <v-list-item-icon class="mr-2">
+                <v-icon small>{{ act.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ act.text }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </template>
+    </v-grid>
+  </div>
+</template>
+
+<script>
+import ResizeMixin from '@/mixins/Resize'
+import TooltipMixin from '@/mixins/Tooltip'
+import VGrid from '@/components/grid'
+import VFormBuilder from '@/components/builder/VFormBuilder'
+import { getObjectValueByPath } from 'vuetify/lib/util/helpers'
+export default {
+  components: {
+    VGrid,
+    VFormBuilder,
+  },
+  mixins: [ResizeMixin, TooltipMixin],
+  props: {
+    headers: {
+      type: Array,
+      default: () => [],
+    },
+    actions: {
+      type: Array,
+      default: () => [],
+    },
+    filterItems: {
+      type: Array,
+      default: () => [],
+    },
+    searchField: {
+      type: String,
+      default: 'name',
+    },
+    action: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      //filter section
+      search: '',
+      showFilter: true,
+      //grid
+      selectedItem: null,
+      loadingItems: false,
+      serverItemsLength: 0,
+      gridOptions: {
+        page: 1,
+        itemsPerPage: 30,
+        sortBy: [],
+        sortDesc: [],
+      },
+      items: [],
+    }
+  },
+  computed: {},
+  watch: {
+    '$route.query': {
+      handler(query) {
+        this.updatePageConfig(query)
+        this.fetchRecords()
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    //
+    updatePageConfig(query) {
+      const { page, pageSize } = query
+      this.gridOptions.page = page ? parseInt(page) : 1
+      this.gridOptions.itemsPerPage = pageSize ? parseInt(pageSize) : 30
+      this.search = getObjectValueByPath(query, this.searchField)
+    },
+    buildApiQuery() {
+      const query = {
+        sort: this.gridOptions.sortBy[0],
+        page: this.gridOptions.page,
+        pageSize: this.gridOptions.itemsPerPage,
+      }
+      const filterKey = `filter[${this.searchField}]`
+      query[filterKey] = this.search
+      return query
+    },
+    buildRouteQuery() {
+      const filter = {}
+      filter[this.searchField] = this.search
+      return {
+        ...filter,
+        sort: this.gridOptions.sortBy[0],
+        page: this.gridOptions.page,
+        pageSize: this.gridOptions.itemsPerPage,
+      }
+    },
+    fetchRecords() {
+      const params = this.buildApiQuery()
+      this.loadingItems = true
+      this.items = []
+      this.$store
+        .dispatch(this.action, params)
+        .then(({ data, meta }) => {
+          this.items = data
+          this.serverItemsLength = meta.total
+          this.loadingItems = false
+        })
+        .catch(() => {
+          this.loadingItems = false
+        })
+    },
+    // filter
+    handleRefreshItem() {
+      this.fetchRecords()
+    },
+    handleResetFilter() {
+      this.$router.replace({
+        path: this.$route.path,
+        query: {},
+      })
+    },
+    handleApplyFilter() {
+      const query = this.buildRouteQuery()
+      console.log(query)
+      query.t = Date.now()
+      this.$router.replace({
+        path: this.$route.path,
+        query: query,
+      })
+    },
+    handleClear() {
+      this.$router.replace({
+        path: this.$route.path,
+        query: {},
+      })
+    },
+  },
+}
+</script>
--- src/views/acl/UserList.vue
+++ src/views/acl/UserList.vue
@@ -3,83 +3,11 @@
     <v-container>
       <v-row>
         <v-col cols="12">
-          <v-sheet class="grid-filter">
-            <v-toolbar flat>
-              <v-text-field
-                v-model="filter.name"
-                text
-                solo
-                flat
-                :prepend-icon="
-                  showFilter ? 'mdi-filter-variant-plus' : 'mdi-filter-variant'
-                "
-                append-icon="mdi-magnify"
-                :placeholder="__('search')"
-                hide-details
-                clearable
-                @keyup.enter="handleApplyFilter"
-                @click:append="handleApplyFilter"
-                @click:prepend="showFilter = !showFilter"
-                @click:clear="handleClear"
-              />
-              <v-btn icon @click="handleRefreshItem">
-                <v-icon>mdi-refresh</v-icon>
-              </v-btn>
-              <v-btn icon @click="handleCreateItem">
-                <v-icon>mdi-plus</v-icon>
-              </v-btn>
-            </v-toolbar>
-            <v-divider />
-            <v-sheet v-show="showFilter" flat class="grey lighten-4">
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn text @click="handleResetFilter">{{ __('reset') }}</v-btn>
-                <v-btn tile color="primary" @click="handleApplyFilter">{{
-                  __('apply')
-                }}</v-btn>
-              </v-card-actions>
-            </v-sheet>
-          </v-sheet>
-          <v-grid
-            :loading="loadingItems"
+          <list-grid
             :headers="headers"
-            :items="items"
-            :footer-props="{ itemsPerPageOptions: [15, 30, 50] }"
-            :server-items-length="serverItemsLength"
-            :options.sync="gridOptions"
-            item-key="id"
-            show-select
-            @update:items-per-page="handleApplyFilter"
-            @update:page="handleApplyFilter"
-            @update:sort-by="handleApplyFilter"
-          >
-            <template #[`item.action`]="{ item }">
-              <v-menu>
-                <template #activator="{ on: menu }">
-                  <v-tooltip bottom>
-                    <template #activator="{ on: tooltip }">
-                      <v-btn icon v-on="onTooltip({ ...tooltip, ...menu })">
-                        <v-icon>mdi-dots-vertical</v-icon>
-                      </v-btn>
-                    </template>
-                    <span>{{ __('action') }}</span>
-                  </v-tooltip>
-                </template>
-                <v-list class="pa-0" dense>
-                  <v-list-item
-                    v-for="action in actions"
-                    :key="action.text"
-                    @click="action.click(item)"
-                  >
-                    <v-list-item-icon class="mr-2">
-                      <v-icon small>{{ action.icon }}</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title>{{ action.text }}</v-list-item-title>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </template>
-          </v-grid>
+            action="fetchUsers"
+            search-field="username"
+          />
         </v-col>
       </v-row>
     </v-container>
@@ -89,34 +17,17 @@
 <script>
 import ResizeMixin from '@/mixins/Resize'
 import TooltipMixin from '@/mixins/Tooltip'
-import VGrid from '@/components/grid'
 import FormUser from '@/components/form/acl/FormUser'
+import ListGrid from '@/components/list/ListGrid'
 import { VAvatar, VImg } from 'vuetify/lib'
 export default {
   name: 'PageUser',
   components: {
-    VGrid,
+    ListGrid,
   },
   mixins: [ResizeMixin, TooltipMixin],
   data() {
     return {
-      //filter section
-      search: '',
-      showFilter: true,
-      filter: {
-        username: null,
-      },
-      //grid
-      selectedItem: null,
-      loadingItems: false,
-      serverItemsLength: 0,
-      sortBy: [],
-      gridOptions: {
-        page: 1,
-        itemsPerPage: 30,
-        sortBy: [],
-        sortDesc: [],
-      },
       headers: [
         {
           text: 'ID',
@@ -168,7 +79,6 @@ export default {
           sortable: false,
         },
       ],
-      items: [],
       actions: [
         {
           text: 'View Item',
@@ -189,78 +99,8 @@ export default {
     }
   },
   computed: {},
-  watch: {
-    '$route.query': {
-      handler(query) {
-        this.updatePageConfig(query)
-        this.fetchRecords()
-      },
-      immediate: true,
-    },
-  },
+  watch: {},
   methods: {
-    //
-    updatePageConfig(query) {
-      const { page, pageSize, username } = query
-      this.gridOptions.page = page ? parseInt(page) : 1
-      this.gridOptions.itemsPerPage = pageSize ? parseInt(pageSize) : 30
-      this.filter.username = username || null
-    },
-    buildApiQuery() {
-      return {
-        sort: this.gridOptions.sortBy[0],
-        page: this.gridOptions.page,
-        pageSize: this.gridOptions.itemsPerPage,
-        'filter[username]': this.filter.name,
-      }
-    },
-    buildRouteQuery() {
-      return {
-        ...this.filter,
-        sort: this.gridOptions.sortBy[0],
-        page: this.gridOptions.page,
-        pageSize: this.gridOptions.itemsPerPage,
-      }
-    },
-    fetchRecords() {
-      const params = this.buildApiQuery()
-      this.loadingItems = true
-      this.items = []
-      return this.$store
-        .dispatch('fetchUsers', params)
-        .then(({ data, meta }) => {
-          this.items = data
-          this.serverItemsLength = meta.total
-          this.loadingItems = false
-        })
-        .catch(() => {
-          this.loadingItems = false
-        })
-    },
-    // filter
-    handleRefreshItem() {
-      this.fetchRecords()
-    },
-    handleResetFilter() {
-      this.$router.replace({
-        path: this.$route.path,
-        query: {},
-      })
-    },
-    handleApplyFilter() {
-      const query = this.buildRouteQuery()
-      query.t = Date.now()
-      this.$router.replace({
-        path: this.$route.path,
-        query: query,
-      })
-    },
-    handleClear() {
-      this.$router.replace({
-        path: this.$route.path,
-        query: {},
-      })
-    },
     //action
     handleCreateItem() {
       this.$root.$dialog.show({
